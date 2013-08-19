f15b92f6f5e99f9282f56530e9df4c9fc6669749
Lee Newson <lnewson@redhat.com>
2013-08-19 11:06:33 +1000

    Refactored how the Bug Fields were retrieved.

--- src/main/java/com/j2bugzilla/rpc/GetBugField.java
+++ src/main/java/com/j2bugzilla/rpc/GetBugField.java
@@ -1,21 +1,12 @@
 package com.j2bugzilla.rpc;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import com.j2bugzilla.base.BugField;
 import com.j2bugzilla.base.BugzillaMethod;
 
-/**
- * This class allows clients to request a list of all public {@link com.j2bugzilla.base.Comment
- * Comments} made on a specific {@link com.j2bugzilla.base.Bug} in a Bugzilla installation. The
- * {@link com.j2bugzilla.base.Bug} must already exist in the installation.
- *
- * @author Tom
- */
-public class BugFields implements BugzillaMethod {
+public class GetBugField implements BugzillaMethod {
     /**
      * The XML-RPC method Bugzilla will use
      */
@@ -32,22 +23,22 @@ public class BugFields implements BugzillaMethod {
     private Map<Object, Object> hash = new HashMap<Object, Object>();
 
     /**
-     * Creates a new {@link com.j2bugzilla.rpc.BugFields} object for the specified {@link com.j2bugzilla.base.BugField}
+     * Creates a new {@link GetBugField} object for the specified {@link com.j2bugzilla.base.BugField}
      * ID.
      *
      * @param id An integer specifying which {@link com.j2bugzilla.base.BugField} to retrieve details for.
      */
-    public BugFields(final int id) {
+    public GetBugField(final int id) {
         params.put("ids", id);
     }
 
     /**
-     * Creates a new {@link com.j2bugzilla.rpc.BugFields} object for the specified {@link com.j2bugzilla.base.BugField}
+     * Creates a new {@link GetBugField} object for the specified {@link com.j2bugzilla.base.BugField}
      * ID.
      *
      * @param name A String specifying which {@link com.j2bugzilla.base.BugField} to retrieve details for.
      */
-    public BugFields(final String name) {
+    public GetBugField(final String name) {
         params.put("names", name);
     }
 
@@ -57,25 +48,23 @@ public class BugFields implements BugzillaMethod {
      *
      * @return A List of {@link com.j2bugzilla.base.BugField} objects
      */
-    public List<BugField> getBugFields() {
-        List<BugField> bugFieldList = new ArrayList<BugField>();
-
+    public BugField getBugFields() {
+        BugField result = null;
         if (hash.containsKey("fields")) {
             final Object[] bugFields = (Object[]) hash.get("fields");
 
             if (bugFields.length == 0) {
-                return bugFieldList;
+                return result;
             } // Early return to prevent ClassCast
 
             for (final Object o : bugFields) {
                 @SuppressWarnings("unchecked")
                 final Map<String, Object> bugFieldMap = (Map<String, Object>) o;
-                final BugField bugField = new BugField(bugFieldMap);
-                bugFieldList.add(bugField);
+                result = new BugField(bugFieldMap);
             }
         }
 
-        return bugFieldList;
+        return result;
     }
 
     /**
